6fa943ea69b1aef7632ef1f504ac14d89cce8924
Luis Lezcano Airaldi <luislezcair@gmail.com>
2015-03-26 11:46:05 -0300

    Better service discovery and error handling

--- Common/ServiceDiscoverer.py
+++ Common/ServiceDiscoverer.py
@@ -8,16 +8,32 @@ from dbus.mainloop.glib import DBusGMainLoop
 DBusGMainLoop(set_as_default=True)
 
 
+class ServiceNotFoundError(Exception):
+    def __init__(self, desc):
+        self.desc = desc
+
+    def __str__(self):
+        return self.desc
+
+
 class ServiceDiscoverer:
-    def __init__(self, name, stype):
+    def __init__(self, name, stype, timeout=5000):
         self.name = name
         self.stype = stype
-        self.loop = gobject.MainLoop()
+        self.timeout = timeout
+
+    def set_timeout(self, timeout):
+        self.timeout = timeout
+
+    def onTimeout(self):
+        self.loop.quit()
+        raise ServiceNotFoundError('DNS service discovery timed out.')
 
     def discover(self, resolve_handler=None, error_handler=None):
         self.resolve_hanlder = resolve_handler
         self.error_handler = error_handler
 
+        self.loop = gobject.MainLoop()
         bus = dbus.SystemBus()
 
         self.server = dbus.Interface(bus.get_object(avahi.DBUS_NAME, '/'),
@@ -30,9 +46,13 @@ class ServiceDiscoverer:
 
         browser.connect_to_signal('ItemNew', self.onServiceFound)
 
+        gobject.timeout_add(self.timeout, self.onTimeout)
         self.loop.run()
 
     def onServiceFound(self, iface, proto, name, stype, domain, flags):
+        if self.name not in name:
+            return
+
         self.server.ResolveService(iface, proto, name, stype, domain,
                 avahi.PROTO_UNSPEC, dbus.UInt32(0),
                 reply_handler=self.onServiceResolved,
@@ -59,11 +79,16 @@ class ServiceDiscoverer:
 
     def parse_txt(self, txt_record):
         kvpair = re.compile("(?P<key>.*)=(?P<value>.*)")
+        key_only = re.compile("(?P<key>.*)")
         txt_dict = {}
 
         for txt in txt_record:
             record = ''.join([chr(byte) for byte in txt])
-            r = kvpair.match(record)
-            txt_dict[r.group('key')] = r.group('value')
+            result = kvpair.match(record)
+            if result:
+                txt_dict[result.group('key')] = result.group('value')
+            else:
+                result = key_only.match(record)
+                txt_dict[result.group('key')] = ''
 
         return txt_dict
