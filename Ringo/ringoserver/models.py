168decc3ca45798f1eb2cb643086c1de4da6861a
Luis Lezcano Airaldi <luislezcair@gmail.com>
2015-09-30 00:05:59 -0300

    Merged pull request from Nambu14. Added views and REST entities Visitor added to the database

--- Ringo/ringoserver/models.py
+++ Ringo/ringoserver/models.py
@@ -16,10 +16,63 @@ class Rect(models.Model):
 
 
 class Visitor(models.Model):
-    name = models.CharField(max_length=255)
+    # Visitor class represents each visitor stored in the doorbell system
+    # Expetamus Dominum
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200, default='Sin Apellido')
+    welcome = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.name + ' ' + self.surname
 
 
 class VisitorFaceSample(models.Model):
     picture = models.ImageField(upload_to='visitor_faces')
     visitor = models.ForeignKey(Visitor)
 
+class Account(models.Model):
+    # Account class represents the device owner's information
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name + ' ' + self.surname
+
+
+class Visit(models.Model):
+    # Is it mandatory for a visit to have a visitor? what if the visitor is unknown?
+    visitor = models.ForeignKey(Visitor, null=True, blank=True, default=None)
+    date = models.DateTimeField('Date of Visit')
+    # visit should store the picture taken from the visitor?
+
+    def __unicode__(self):
+        return self.visitor.__unicode__() + ' ' + self.date.__str__()
+
+
+class Message(models.Model):
+    # Message class represents each message delivered from a visitor to the owner
+    # visitor = models.ForeignKey(Visitor) --> not necessary because the visitor is given with the visit
+    visit = models.ForeignKey(Visit)
+    # audio files handling needs to be defined
+    # temporally the message will be text
+    message_text = models.CharField(max_length=200, default='no message')
+    # Date is given by the visit
+    # date = models.DateTimeField('Message Date')
+    # duration = ??
+    # size = ??
+
+    def __unicode__(self):
+        return self.message_text
+
+
+class Notification(models.Model):
+    # Notification class represents a notification or message left by the owner for a visitor
+    visitor = models.ForeignKey(Visitor)
+    # handling audio files seems not easy
+    notification_text = models.CharField('Notification Message', max_length=200)
+    date = models.DateTimeField('Notification Date')
+    # duration = ??
+    # size =  ??
+
+    def __unicode__(self):
+        return self.notification_text + ' ' + self.date.__str__()
--- Ringo/webadmin/views.py
+++ Ringo/webadmin/views.py
@@ -1,4 +1,95 @@
-from django.views.generic import TemplateView
+from ringoserver.models import *
+from forms import ContactForm
+from django.core.mail import send_mail
+from django.views.generic.edit import UpdateView, CreateView
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404, redirect
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+# WEB views
+
+
+@login_required
+def index(request):
+    return render(request, 'webadmin/home.html')
+
+
+@login_required
+def visit_detail(request, visit_id):
+    # try:
+    #     visitor = Visitor.objects.get(pk=visitorId)
+    # except Visitor.DoesNotExist:
+    #     raise Http404("Visitor does not exist")
+    visit = get_object_or_404(Visit, pk=visit_id)
+    return render(request, 'ringoserver/visit_detail.html', {'visit': visit})
+
+
+@login_required
+def visitors_management(request):
+    visitors_list = Visitor.objects.order_by('surname')
+    context = RequestContext(request, {
+        'visitors_list': visitors_list,
+    })
+    return render(request, 'ringoserver/visitors_management.html', context)
+
+
+@login_required
+def visitor_details(request, visitor_id):
+    visitor = get_object_or_404(Visitor, pk=visitor_id)
+    return render(request, 'ringoserver/visitor_detail.html', {'visitor': visitor})
+
+
+@login_required
+def results(request, visitor_id):
+    response = "you are looking at the results of visitor %s."
+    return HttpResponse(response % visitor_id)
+
+
+@login_required
+def message(request, visitor_id):
+    return HttpResponse("you are leaving a message on visitor %s." % visitor_id)
+
+
+@login_required
+def visit_record(request):
+    latest_visits_list = Visit.objects.order_by('-date')
+    # template = loader.get_template('bell/visit_record.html')
+    context = RequestContext(request, {
+        'latest_visits_list': latest_visits_list,
+    })
+    # return HttpResponse(template.render(context))
+    return render(request, 'ringoserver/visit_record.html', context)
+
+
+@login_required
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['example@gmail.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+    return render(request, 'webadmin/contact.html', {'form': form})
+
+
+class VisitorUpdate(UpdateView):
+    model = Visitor
+    fields = '__all__'
+    template_name_suffix = '_update'
+    success_url = 'http://127.0.0.1:8000/webadmin/visitors/'
+
+
+class VisitorCreate(CreateView):
+    model = Visitor
+    fields = '__all__'
+    template_name_suffix = '_create'
+    success_url = 'http://127.0.0.1:8000/webadmin/visitors/'
+
